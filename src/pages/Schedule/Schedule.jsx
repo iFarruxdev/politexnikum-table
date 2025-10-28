19fd58331cd7882935f5a216abd090e35df54be0
iFarruxdev <alimovf01@gmail.com>
2025-10-28 13:03:47 +0500

    add course section and seventy

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -221,34 +221,71 @@ const Schedule = () => {
           { subject: "Matematika", time: "12:00 - 13:20", teacher: "Jo'rayeva D", room: "4,10" },
           { subject: "Mehnat muhofazasi", time: "13:30 - 14:50", teacher: "Ergashev O", room: "4,6" },
         ],
-        // TODO Seshanbada qoldim !!!
         Seshanba: [
-          { subject: "Jismoniy tarbiya / CHQBT", time: "08:30 - 09:50", teacher: "Jo'rayev N /Aminjonov M", room: "zal - 4,5" },
-          { subject: "Tikuvchili jixozlari", time: "10:00 - 11:20", teacher: "Qurbonova N", room: "1,18" },
-          { subject: "Ingliz tili", time: "12:00 - 13:20", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
-          { subject: "Tikuv buyumlari tikish tex./ --------", time: "13:30 - 14:50", teacher: "Hamdamova M", room: "1,5" },
+          { subject: "Ingliz tili", time: "08:30 - 09:50", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+          { subject: "Ona tili", time: "10:00 - 11:20", teacher: "Qobilova O", room: "3,11" },
+          { subject: "Tarix", time: "12:00 - 13:20", teacher: "Ro'zimatov A", room: "3,21" },
         ],
         Chorshanba: [
-          { subject: "Rus tili / Informatika", time: "08:30 - 09:50", teacher: "Isaqova N / Djurayeva S / Musayev I", room: "2,41 - 2,6" },
-          { subject: "Tarbiya / Fizika", time: "10:00 - 11:20", teacher: "Murodov A /Saydaliyeva I", room: "3,4 - 4,15" },
-          { subject: "Matematika", time: "12:00 - 13:20", teacher: "Hudoyberdiyeva Sh", room: "4,12" },
+          { subject: "Fizika / Rus tili ", time: "08:30 - 09:50", teacher: "Saydaliyeva I / Isaqova N  / Djurayeva S", room: "4,15 - 2,41" },
+          { subject: "Ingliz tili", time: "10:00 - 11:20", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+          { subject: "Ma'lumotlar bazasi", time: "12:00 - 13:20", teacher: "Ergasheva X", room: "4,9" },
+          { subject: "Axborot texnologilari", time: "13:30 - 14:50", teacher: "Yunusaliyev A", room: "3,25" },
         ],
         Payshanba: [
-          { subject: "Mehnat muhofazasi", time: "08:30 - 09:50", teacher: "Imomova M", room: "1,5" },
-          { subject: "Ona tili", time: "10:00 - 11:20", teacher: "Qobilova O", room: "3,12" },
-          { subject: "Loyhalash", time: "12:00 - 13:20", teacher: "Imomova M", room: "1,5" },
-          { subject: "Ingliz tili", time: "13:30 - 14:50", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+          { subject: "Tarbiya / Jismoniy tarbiya", time: "08:30 - 09:50", teacher: "Murodov A /Jo'rayev N", room: "3,4 - zal" },
+          { subject: "Ingliz tili", time: "10:00 - 11:20", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+          { subject: "Raqamli axborot", time: "12:00 - 13:20", teacher: "Ergasheva X", room: "4,9" },
         ],
         Juma: [
-          { subject: "Tikuvchilik buyumlari tikish O'A / Eskiz asosida tikish O'A", time: "08:30 - 09:50", teacher: "Turg'unova S", room: "2,7" },
-          { subject: "Tikuvchilik buyumlari tikish O'A / Eskiz asosida tikish O'A", time: "10:00 - 11:20", teacher: "Turg'unova S", room: "2,7" },
-          { subject: "Tikuvchilik buyumlari tikish O'A / Eskiz asosida tikish O'A", time: "12:00 - 13:20", teacher: "Turg'unova S", room: "2,7" },
-          { subject: "Ma'naviyat soati", time: "13:30 - 14:10", teacher: "Sultonova O'", room: "3,7" },
+          { subject: "Raqamli axborot qayta ishlash O'A /CHQBT", time: "08:30 - 09:50", teacher: "Ergasheva X /Nazarov A", room: "4,9 - 4,13" },
+          { subject: "Raqamli axborot qayta ishlash O'A / Raqamli axborot", time: "10:00 - 11:20", teacher: "Ergasheva X /Ergasheva X", room: "4,9 - 4,9" },
+          { subject: "Raqamli axborot qayta ishlash O'A / Geografiya", time: "12:00 - 13:20", teacher: "Ergasheva X / Qurbonova X", room: "4,9" },
+          { subject: "Ma'naviyat soati", time: "13:30 - 14:10", teacher: "Murodov A", room: "3,4" },
         ],
         Shanba: [
-          { subject: "Ingliz tili", time: "08:30 - 09:50", teacher: "Ohunova Z / Mahamadjonova Sh ", room: "3,32" },
-          { subject: "Tikuv maxsulot konsturuksiyalash / Tikuvchilik buyumlari tayyorlash", time: "10:00 - 11:20", teacher: "Imomova M", room: "1,5" },
-          { subject: "Ingliz tili", time: "12:00 - 13:20", teacher: "Djurayeva N", room: "3,32" },
+          { subject: "Kompyuter ofis qurulmalari", time: "08:30 - 09:50", teacher: "Ergasheva X ", room: "4,9" },
+          { subject: "Ingliz tili", time: "10:00 - 11:20", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+          { subject: "Informatika", time: "12:00 - 13:20", teacher: "Roziqov J / Musayev I", room: "2,6 - 3,10" },
+        ]
+      }
+    },
+    {
+      groupNumber: "7-25",
+      groupName: "Raqamli axborotlarni qayta ishlash ustasi",
+      schedule: {
+        Dushanba: [
+          { subject: "Axborot xavfsizligi", time: "08:30 - 09:50", teacher: "Ergasheva X", room: "4,9" },
+          { subject: "Informatika", time: "10:00 - 11:20", teacher: "Roziqov J / Musayev I", room: "2,6 - 3,10" },
+          { subject: "Ingliz tili", time: "12:00 - 13:20", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+          { subject: "Axborot texnologilari", time: "13:30 - 14:50", teacher: "Sheraliyev S", room: "1,7" },
+        ],
+        Seshanba: [
+          { subject: "Raqamli axborot qayta ishlash O'A /Tarbiya", time: "08:30 - 09:50", teacher: "Qurbonov D / Murodov A", room: "2,10 - 3,4" },
+          { subject: "Raqamli axborot qayta ishlash O'A/ CHQBT", time: "10:00 - 11:20", teacher: "Qurbonov D / Nazarov A", room: "2,10 - 4,13" },
+          { subject: "Raqamli axborot qayta ishlash O'A / Jismoniy tarbiya", time: "12:00 - 13:20", teacher: "Qurbonov D / Jo'rayev N", room: "2,10 - zal" },
+          { subject: "Raqamli axborot qayta ishlash", time: "13:30 - 14:50", teacher: "Ergasheva X", room: "4,9" },
+        ],
+        Chorshanba: [
+          { subject: "Tarix", time: "08:30 - 09:50", teacher: "Ro'zimatov A", room: "3,21" },
+          { subject: "Mehnat muhofazasi", time: "10:00 - 11:20", teacher: "Ergashev O", room: "4,6" },
+          { subject: "Ingliz tili", time: "12:00 - 13:20", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+        ],
+        Payshanba: [
+          { subject: "Ma'lumotlar bazasi", time: "08:30 - 09:50", teacher: "Sheraliyev S", room: "1,7" },
+          { subject: "Matematika", time: "10:00 - 11:20", teacher: "Jo'rayeva D", room: "4,10" },
+          { subject: "Ingliz tili", time: "12:00 - 13:20", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+        ],
+        Juma: [
+          { subject: "Ingliz tili", time: "08:30 - 09:50", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+          { subject: "Ingliz tili", time: "10:00 - 11:20", teacher: "Ohunova Z / Mahamadjonova Sh", room: "3,32" },
+          { subject: "Geografiya / Raqamli axborot", time: "12:00 - 13:20", teacher: "Qurbonov D / Ergasheva X", room: "4,10 - 4,9" },
+          { subject: "Ma'naviyat soati", time: "13:30 - 14:10", teacher: "Abdurahimov A", room: "4,14" },
+        ],
+        Shanba: [
+          { subject: "Kompyuter ofis qurulmalari", time: "08:30 - 09:50", teacher: "Sheraliyev S", room: "1,7" },
+          { subject: "Ona tili", time: "10:00 - 11:20", teacher: "Qobilova O", room: "3,11" },
+          { subject: "Fizika / Rus tili", time: "12:00 - 13:20", teacher: "Saydaliyeva I / Isaqova N -Djurayeva S", room: "4,15 - 2,41" },
         ]
       }
     },
@@ -291,7 +328,7 @@ const Schedule = () => {
       "Bolalar kiyimini tikish O'A": 'bg-indigo-50 border-indigo-200 hover:bg-indigo-100',
       'Tarbiya / Fizika': 'bg-teal-50 border-teal-200 hover:bg-teal-100',
       'Informatika / Tarbiya': 'bg-lime-50 border-lime-200 hover:bg-lime-100',
-      'Career Development': 'bg-orange-50 border-orange-200 hover:bg-orange-100',
+      "Ma'lumotlar bazasi": 'bg-orange-50 border-orange-200 hover:bg-orange-100',
       'Code Review Session': 'bg-slate-50 border-slate-200 hover:bg-slate-100',
       'Bolalar kiyimini tikish': 'bg-cyan-50 border-cyan-200 hover:bg-cyan-100',
       'Rasm va  kostyum b. grafikasi / -------': 'bg-pink-50 border-pink-200 hover:bg-pink-100',
@@ -300,23 +337,23 @@ const Schedule = () => {
       'Brand Management': 'bg-violet-50 border-violet-200 hover:bg-violet-100',
       'Rus tili / Informatika': 'bg-red-50 border-red-200 hover:bg-red-100',
       'Copywriting': 'bg-amber-50 border-amber-200 hover:bg-amber-100',
-      'Paid Advertising': 'bg-green-50 border-green-200 hover:bg-green-100',
-      'Influencer Marketing': 'bg-rose-50 border-rose-200 hover:bg-rose-100',
+      'Geografiya / Raqamli axborot': 'bg-green-50 border-green-200 hover:bg-green-100',
+      'Axborot xavfsizligi': 'bg-rose-50 border-rose-200 hover:bg-rose-100',
       'Market Research': 'bg-slate-50 border-slate-200 hover:bg-slate-100',
       'Tikuv maxsulot konsturuksiyalash / Tikuvchilik buyumlari tayyorlash': 'bg-orange-50 border-orange-200 hover:bg-orange-100',
-      'Google Ads': 'bg-sky-50 border-sky-200 hover:bg-sky-100',
+      'Fizika / Rus tili': 'bg-sky-50 border-sky-200 hover:bg-sky-100',
       'Loyhalash': 'bg-lime-50 border-lime-200 hover:bg-lime-100',
-      'Content Calendar': 'bg-yellow-50 border-yellow-200 hover:bg-yellow-100',
+      "Raqamli axborot qayta ishlash O'A / Jismoniy tarbiya": 'bg-yellow-50 border-yellow-200 hover:bg-yellow-100',
       'Tikuvchilik materallari': 'bg-blue-50 border-blue-200 hover:bg-blue-100',
       'Instagram Strategy': 'bg-fuchsia-50 border-fuchsia-200 hover:bg-fuchsia-100',
       'Tikuvchili jixozlari': 'bg-cyan-50 border-cyan-200 hover:bg-cyan-100',
-      'Analytics Tools': 'bg-emerald-50 border-emerald-200 hover:bg-emerald-100',
-      'Campaign Planning': 'bg-teal-50 border-teal-200 hover:bg-teal-100',
-      'E-commerce Marketing': 'bg-green-50 border-green-200 hover:bg-green-100',
+      "Raqamli axborot qayta ishlash O'A /Tarbiya": 'bg-emerald-50 border-emerald-200 hover:bg-emerald-100',
+      'Kompyuter ofis qurulmalari': 'bg-teal-50 border-teal-200 hover:bg-teal-100',
+      'Raqamli axborot qayta ishlash': 'bg-green-50 border-green-200 hover:bg-green-100',
       "Tarbiya / Kiyim mayda deta i/b O'A": 'bg-rose-50 border-rose-200 hover:bg-rose-100',
-      'Conversion Optimization': 'bg-amber-50 border-amber-200 hover:bg-amber-100',
+      "Raqamli axborot qayta ishlash O'A/ CHQBT": 'bg-amber-50 border-amber-200 hover:bg-amber-100',
       'Tikuvchilik materialshunosligi': 'bg-violet-50 border-violet-200 hover:bg-violet-100',
-      'Portfolio Presentation': 'bg-orange-50 border-orange-200 hover:bg-orange-100'
+      'Axborot texnologilari': 'bg-orange-50 border-orange-200 hover:bg-orange-100'
     };
     return colorMap[subject] || 'bg-gray-50 border-gray-200 hover:bg-gray-100';
   };
